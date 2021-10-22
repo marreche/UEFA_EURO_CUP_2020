06d9e301f195851f5c34ebdd29c222627831b18b
Mauricio Arrechea <marrechea@pop-os.localdomain>
2021-10-22 03:57:44 +0200

    More changes

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,9 @@
 FROM python:3.9.7
 
 ADD ./ /Euro2020api
+
+WORKDIR /Euro2020api
+
+RUN pip install -r requirements.txt
+
+CMD ["python","Euro_cup_2020_api/main.py"]
--- Euro_cup_2020_api/main.py
+++ Euro_cup_2020_api/main.py
@@ -1,5 +1,7 @@
 from src.controllers.general_endpoints import app
 from src.controllers.stage_endpoints import app
 from src.controllers.team_endpoints import app
+import os
 
-app.run(debug=True)
+port = os.getenv("PORT") or 5500
+app.run(debug=True, port=port, host="0.0.0.0")
